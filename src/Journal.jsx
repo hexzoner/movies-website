ca3a2a1e5ce9acd296a9088ea6b0676d3cc4970a
hexzoner <hexzon@gmail.com>
2024-07-13 12:41:07 +0200

    journal styling

--- src/Journal.jsx
+++ src/Journal.jsx
@@ -6,10 +6,10 @@ export default function Journal() {
   const { favorites, onMovieDetails } = useContext(MovieContex);
 
   return (
-    <div className="bg-[#16181e] h-[100vh]">
-      <section className="mx-auto max-w-[1120px] container py-12 ">
-        <h1 className="font-bold text-5xl text-center mb-12 text-white">My Journal</h1>
-        <div id="cards-container" className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="bg-neutral min-h-screen">
+      <section className="mx-auto max-w-[1120px] container py-8 ">
+        <h1 className="font-bold text-4xl text-left mb-12 text-white">My Journal</h1>
+        <div id="cards-container" className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 mb-6">
           {favorites.map((x) => {
             return <MovieCard onMovieDetails={onMovieDetails} movie={x} />;
           })}
